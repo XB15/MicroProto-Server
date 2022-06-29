aeddfc466526c62b3cf1c706e114b37ec4d101b9
Smeltie <martijnklerks123@gmail.com>
2022-06-29 22:48:41 +0200

    added logo

--- xb15-web/src/components/Navbar.vue
+++ xb15-web/src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav>
-        <v-app-bar dark app color="grey darken-4" :clipped-left="$vuetify.breakpoint.smAndUp">
+        <v-app-bar dark app color="grey darken-4" :clipped="$vuetify.breakpoint.smAndUp">
             <v-app-bar-nav-icon @click="drawer = !drawer" class="hidden-sm-and-up">
                <v-icon>mdi-menu</v-icon>
             </v-app-bar-nav-icon>
@@ -8,22 +8,26 @@
                 <span>MicroProto</span>
             </v-toolbar-title>
             <v-spacer></v-spacer>
+            <v-btn fab depressed small color="grey darken-4">
+                <v-icon>mdi-bell-outline</v-icon>
+            </v-btn>
             <v-btn fab depressed small color="grey darken-4" @click="drawer_right = !drawer_right">
                 <v-icon>mdi-power</v-icon>
             </v-btn>
         </v-app-bar>
         
         <v-navigation-drawer
-         :clipped="$vuetify.breakpoint.smAndUp" 
          :permanent="$vuetify.breakpoint.smAndUp" 
          v-model="drawer" 
          dark 
          app 
          class="grey darken-2" 
          mini-variant 
-         :mini-variant-width="80">
+         :mini-variant-width="60">
+         
             <v-list>
-                <v-list-item v-for="link in links_right" :key="link.text" router :to="link.route">
+                <v-img src="@/assets/Logo_color.png" height="56.2"></v-img>
+                <v-list-item v-for="link in links_left" :key="link.text" router :to="link.route">
                     <v-list-item-action>
                         <v-icon>{{ link.icon }}</v-icon>
                     </v-list-item-action>
@@ -53,10 +57,12 @@ export default {
         return {
             drawer: false,
             drawer_right:false,
-            links_leftt: [
-                { icon: 'mdi-power', text: 'Power-Off', route: 'poweroffcode'},
-                { icon: 'mdi-restart', text: 'Reboot', route: 'rebootcode'},
-                { icon: 'mdi-server', text: 'System-Status', route: 'status'}
+            links_left: [
+                { icon: 'mdi-home', text: 'Home', route: 'home'},
+                { icon: 'mdi-lightbulb-on', text: 'Ears', route: 'ears'},
+                { icon: 'mdi-fan', text: 'Fan', route: 'fan'},
+                { icon: 'mdi-cog', text: 'Settings', route: 'settings'},
+                { icon: 'mdi-download', text: 'Updates', route: 'update'}
             ],
             links_right: [
                 { icon: 'mdi-power', text: 'Power-Off', route: 'poweroffcode'},
