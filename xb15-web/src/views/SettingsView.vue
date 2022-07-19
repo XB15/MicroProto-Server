7614eb6abee4b0102c9f5080c4d7b9618f02615a
Martijn <martijn@localhost.localdomain>
2022-07-19 22:08:12 +0200

    Switched to openSuse everythings fixed now

--- xb15-web/src/views/SettingsView.vue
+++ xb15-web/src/views/SettingsView.vue
@@ -3,8 +3,40 @@
     <h1 class="white--text mx-4 mt-4">Settings</h1>
     <v-container>
       <v-row>
-        <v-col></v-col>
+        <v-col>
+          <h3 class="white--text">Ear settings</h3>
+        </v-col>
       </v-row>
+      <v-card dark>
+        <v-row align="center">
+          <v-col cols="auto">
+            <h4 class="px-3">Primary Ear</h4>
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col cols="auto">
+            <h4 class="pr-2">left</h4>
+          </v-col>
+          <v-col cols="2" md="1">
+            <v-switch v-model="earSide" inset color="#bc59f6"></v-switch>
+          </v-col>
+          <v-col cols="auto">
+            <h4 class="pr-3">right</h4>
+          </v-col>
+        </v-row>
+        <v-divider></v-divider>
+        <v-row align="center">
+          <v-col cols="auto">
+            <h4 class="px-3">LEDs Per Ear</h4>
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col cols="2">
+            <v-text-field class="centered-input" maxlength="2" v-model="ledCount"> </v-text-field>
+          </v-col>
+          <v-col cols="auto">
+            <h4 class="pr-3">LEDs</h4>
+          </v-col>
+        </v-row>
+      </v-card>
     </v-container>
   </div>
 </template>
@@ -12,5 +44,17 @@
 <script>
 export default {
   name: "Settings",
+  data() {
+    return {
+      earSide: false,
+      ledCount: "24"
+    };
+  },
 };
 </script>
+
+<style>
+.centered-input input {
+  text-align: center;
+}
+</style>
