1fb3afa93a258159789b1f00151e0878761a775b
Smeltie <martijnklerks123@gmail.com>
2022-08-01 20:15:47 +0200

    removed some files

--- xb15-web/src/views/ScreensView.vue
+++ xb15-web/src/views/ScreensView.vue
@@ -14,24 +14,26 @@
       <v-row class="justify-left pt-8">
         <v-col cols="4" md="3" v-for="item in gallery" :key="item.src">
           <v-card hover>
-            <v-img :src="'/img/' + item.src" aspect-ratio="1">
+            <v-img :src="'/img/' + item.src" class="pixelated">
               <v-img
                 src="@/assets/overlay_gallery_1.png"
                 class="pt-1"
-                aspect-ratio="1"
                 v-if="item.status === 1"
               ></v-img>
               <v-img
                 src="@/assets/overlay_gallery_2.png"
                 class="pt-1"
-                aspect-ratio="1"
                 v-if="item.status === 2"
               ></v-img>
               <v-img
                 src="@/assets/overlay_gallery_3.png"
                 class="pt-1"
-                aspect-ratio="1"
                 v-if="item.status === 3"
+              ></v-img>
+              <v-img
+                src="@/assets/overlay_gallery_4.png"
+                class="pt-1"
+                v-if="item.status === 4"
               ></v-img>
             </v-img>
           </v-card>
@@ -51,7 +53,7 @@ export default {
         { src: "Logo_color.184208e9.png", status: 0 },
         { src: "Logo_color.184208e9.png", status: 3 },
         { src: "Logo_color.184208e9.png", status: 0 },
-        { src: "Logo_color.184208e9.png", status: 0 },
+        { src: "Logo_color.184208e9.png", status: 4 },
         { src: "Logo_color.184208e9.png", status: 0 },
         { src: "Logo_color.184208e9.png", status: 2 },
         { src: "Logo_color.184208e9.png", status: 0 },
@@ -61,3 +63,15 @@ export default {
   },
 };
 </script>
+
+<style>
+.pixelated {
+  image-rendering: optimizeSpeed; /* STOP SMOOTHING, GIVE ME SPEED  */
+  image-rendering: -moz-crisp-edges; /* Firefox                        */
+  image-rendering: -o-crisp-edges; /* Opera                          */
+  image-rendering: -webkit-optimize-contrast; /* Chrome (and eventually Safari) */
+  image-rendering: pixelated; /* Chrome */
+  image-rendering: optimize-contrast; /* CSS3 Proposed                  */
+  -ms-interpolation-mode: nearest-neighbor; /* IE8+                           */
+}
+</style>
